3d6ec1523c361912730b7d76e94603f8328ea725
ttamx <teetat.tata@gmail.com>
2025-04-11 22:40:59 +0700

    feat: multiple user & contest

--- Extension/content.js
+++ Extension/content.js
@@ -1,26 +1,31 @@
+const baseURL = window.location.href.split(/(\/tasks)|(\/communication)|(\/documentation)|(\/testing)/)[0].replace(/\/+$/g, '');
+const user = document.querySelector("em")?.textContent;
+
 var parser = new DOMParser();
 
 var score = new Map();
 var fullScore = new Map();
 
-const storageCache = { count: 0 };
-const initStorageCache = chrome.storage.sync.get().then((items) => {
-  Object.assign(storageCache, items);
-});
-
 (async function() {
+    if(!user) return;
+    
+    const storageCache = { count: 0 };
     try {
-        await initStorageCache;
+        await chrome.storage.sync.get().then((items) => {
+            Object.assign(storageCache, items);
+        });
     } catch {}
 
     storageCache.count++;
     chrome.storage.sync.set(storageCache);
 
-    data = await chrome.storage.sync.get(["score", "fullScore"])
-    score = new Map(Object.entries(data.score));
-    fullScore = new Map(Object.entries(data.fullScore));
+    const scoreKey = `${baseURL}_${user}_score`;
+    const fullScoreKey = `${baseURL}_${user}_fullScore`;
+    data = await chrome.storage.sync.get([scoreKey, fullScoreKey]);
+    if(data[scoreKey]) score = new Map(Object.entries(data[scoreKey]));
+    if(data[fullScoreKey]) fullScore = new Map(Object.entries(data[fullScoreKey]));
 
-    var elements = document.querySelectorAll(".nav-list li");
+    const elements = document.querySelectorAll(".nav-list li");
 
     updateSidebar();
 
@@ -29,27 +34,27 @@ const initStorageCache = chrome.storage.sync.get().then((items) => {
             const names = elements[i].getElementsByTagName("span");
             const taskName = (names.length > 0 ? names[0] : elements[i]).textContent.trim();
             const url = elements[i + 2].getElementsByTagName("a")[0].href;
+            
             const response = await fetch(url);
             const htmlContent = await response.text();
             const parsedHTML = parser.parseFromString(htmlContent, 'text/html');
+            
             const result = getScore(parsedHTML).split("/");
             score.set(taskName, parseInt(result[0]));
             fullScore.set(taskName, parseInt(result[1]));
         } catch {}
     }
-    chrome.storage.sync.set({
-        score: Object.fromEntries(score),
-        fullScore: Object.fromEntries(fullScore)}
-    );
+
     updateSidebar();
+
+    object = new Object();
+    object[scoreKey] = Object.fromEntries(score);
+    object[fullScoreKey] = Object.fromEntries(fullScore);
+    chrome.storage.sync.set(object);
 })()
 
-chrome.storage.onChanged.addListener((changes, namespace) => {
-    if(namespace == "score" && changes?.newValue) updateSidebar();
-});
-
 function getScore(parsedHtml) {
-    var element = parsedHtml.getElementsByClassName("task_score_container")[0].getElementsByClassName("score")[0];
+    const element = parsedHtml.getElementsByClassName("task_score_container")[0].getElementsByClassName("score")[0];
     return element.textContent.trim();
 }
 
@@ -63,9 +68,14 @@ function updateSidebar(){
             currentScore = score.get(taskName);
             currentFullScore = fullScore.get(taskName);
             element.innerHTML = `
-                <span>${taskName}</span>
+                <span>
+                    ${taskName}
+                </span>
                 <span style="float:right">
-                    <div class="task_score score_${currentScore == currentFullScore ? '100' : currentScore > 0 ? '0_100' : '0'}" style="border-radius:4px; padding-left:4px; padding-right:4px; color:black">
+                    <div
+                        class="task_score score_${currentScore == currentFullScore ? '100' : currentScore > 0 ? '0_100' : '0'}"
+                        style="border-radius:4px; padding-left:4px; padding-right:4px; color:black"
+                    >
                         ${currentScore} / ${currentFullScore}
                     </div>
                 </span>`;
